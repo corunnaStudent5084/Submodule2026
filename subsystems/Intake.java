e6324e562644a1d043b3407a7e5693a9ee68da0c
corunnaStudent5084 <nicholasrubenmars+student5084@gmail.com>
2026-03-23 20:42:39 -0700

    Switched which motor the controllers are plugged into

--- subsystems/Intake.java
+++ subsystems/Intake.java
@@ -41,9 +41,9 @@ public class Intake extends SubsystemBase{
     // private final RelativeEncoder Indexer_Encoder = Indexer.getEncoder();
 
     // black motors
-    private final SparkMax ShooterMotor = new SparkMax(15, MotorType.kBrushless);
+    private final SparkMax ShooterMotor = new SparkMax(17, MotorType.kBrushless);
     private final RelativeEncoder Shooter_encoder = ShooterMotor.getEncoder();
-    private final SparkMax Indexer = new SparkMax(17,MotorType.kBrushless);
+    private final SparkMax Indexer = new SparkMax(15,MotorType.kBrushless);
 
     // maybe shared?
     private final SparkMax Intake = new SparkMax(14, MotorType.kBrushless);
@@ -132,7 +132,7 @@ public class Intake extends SubsystemBase{
     }
 
     public void Indexer_motorSpeed(double speed){
-        Indexer.set(-speed);
+        Indexer.set(speed);
     }
 
     public void MoveExstendIntakeSolenoid(boolean on){
